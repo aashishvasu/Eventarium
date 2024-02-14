46c6e1878e7df920ad3a01eee30a4e3e9acab8c9
Aashish Vasudevan <aashishvasu@gmail.com>
2024-02-14 11:20:19 +0530

    Added enum to track status of event

---
+++ include/ev/Enums.h
@@ -0,0 +1,14 @@
+﻿// Aashish Vasudevan
+
+#ifndef ENUMS_H
+#define ENUMS_H
+
+// Enum for representing the status of events
+enum class EventStatus
+{
+	ES_ACTIVE,
+	ES_PAUSED,
+	ES_UNBOUND
+};
+
+#endif // ENUMS_H
